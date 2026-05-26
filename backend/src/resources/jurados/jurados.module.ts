02faa8802f7b8dea3782e07005af87f03bbd2ee5
Shurecito <jdmg206@gmail.com>
2026-05-25 19:00:39 -0500

    fix(jurados): import CommonModule + PrismaModule into JuradosModule for AuthGuard DI

--- backend/src/resources/jurados/jurados.module.ts
+++ backend/src/resources/jurados/jurados.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CommonModule } from '../../common/common.module.js';
+import { PrismaModule } from '../../prisma/prisma.module.js';
 import { JuradosController } from './jurados.controller.js';
 import { JuradosService } from './jurados.service.js';
 
 @Module({
+  imports: [CommonModule, PrismaModule],
   controllers: [JuradosController],
   providers: [JuradosService],
   exports: [JuradosService],
